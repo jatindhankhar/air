4a4ba7476b6acf0c0e978ffea4b484da1d07cb81
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2017-10-07 23:59:44 +0530

    Add basic background map overlay

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,34 @@
+//Try HTML5 geolocation.
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        console.log(pos);
+        // infoWindow.setPosition(pos);
+        // infoWindow.setContent('Location found.');
+        //infoWindow.open(map);
+        //map.setCenter(pos);
+        init_map(pos["lat"], pos["lng"]);
+    }, function() {
+        //handleLocationError(true, infoWindow, map.getCenter());
+    });
+} else {
+    // Browser doesn't support Geolocation
+    //handleLocationError(false, infoWindow, map.getCenter());
+}
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    //infoWindow.setPosition(pos);
+    //infoWindow.setContent(browserHasGeolocation ?
+    //   'Error: The Geolocation service failed.' :
+    //  'Error: Your browser doesn\'t support geolocation.');
+    //infoWindow.open(map);
+}
+
+
 $(function() {
 
     $(".dropdown-menu a").click(function() {
@@ -36,4 +67,11 @@ $("#target").submit(
 
 function print_raw_json(data) {
     $('#raw-json').text(JSON.stringify(data, null, 4));
+}
+
+function init_map(lat, lng) {
+    L.mapbox.accessToken = 'pk.eyJ1IjoiamF0aW5kaGFua2hhciIsImEiOiJjajhnN3B6azUwOWIyMzJuNmNvMWlsbXg0In0.8hqJ0HFxyROInUXSN6h5KQ';
+    var map = L.mapbox.map('map-container', 'mapbox.streets')
+        .setView([lat, lng], 9);
+
 }
